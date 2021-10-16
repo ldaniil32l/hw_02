1309ec8d78443fcae7ca0f4289170c48162a3e72
daniil <popov.dan.2016@gmail.com>
2021-10-16 22:04:28 +0300

    Initial commit

---
+++ Multiplication/Multiplication.sol
@@ -0,0 +1,24 @@
+pragma ton-solidity >= 0.35.0;
+pragma AbiHeader expire;
+
+contract Multiplication {
+
+	uint public prod = 1;
+
+	constructor() public {
+		require(tvm.pubkey() != 0, 101);
+		require(msg.pubkey() == tvm.pubkey(), 102);
+		tvm.accept();
+	}
+
+	modifier checkOwnerAndAccept {
+		require(msg.pubkey() == tvm.pubkey(), 102);
+		tvm.accept();
+		_;
+	}
+
+	function mul(uint value) public checkOwnerAndAccept {
+		require(value <= 10 && value >= 1, 103);
+        prod *= value;
+	}
+}
